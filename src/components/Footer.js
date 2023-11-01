e6301f7695a37757ffdbbaa84ac83f815da347f6
abbyyT <abitesfahunn@gmail.com>
2023-11-01 21:39:17 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
           maxWidth="1024px"
           height={16}
         >
-          <p>Pete • © 2022</p>
+          <p>Abi• © 2023</p>
         </Flex>
       </footer>
     </Box>
