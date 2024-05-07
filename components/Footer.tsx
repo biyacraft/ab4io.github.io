dd331a1d0fea30f2a1e463ca8e8b358c37f008cc
tesfaa abi <ayalkbettesfahun@gmail.com>
2024-05-07 09:59:47 +0300

    update on homepage

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,25 @@
 import Link from "next/link";
 
 
-
 export default function Footer(){
     return(
-        <div className="py-10 text-center text-sm">
+        <div className="py-10  text-center text-sm">
             <hr />
-            <p>
+            <p className="">
                 The{" "}<Link
                 href="#"
                 target="_blank"
                 className="underline-offset-2 hover:text-emerald-500 hover:underline">
                     ab4i40
-                </Link>{" "}Blog
+                </Link>{" "}Blog{" "} {" "}    
+                <Link 
+                href="https://drive.google.com/file/d/10ZnQoXCbRWS5BB0jU8TPq-AWZEBslLij/view?usp=sharing"
+                className="underline-offset-2 hover:text-emerald-500 hover:underline"
+                target="_blank">
+                   {" "} Download CV</Link>
             </p>
+            
+            
         </div>
     )
 }
