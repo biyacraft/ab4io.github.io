df2554ea8521eef1e991ac7a3c8a7b6803f7336d
ab4io <ayalkbettesfahun@gmail.com>
2024-07-24 01:48:01 +0300

    Update page.tsx

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,4 +1,4 @@
-
+/*
 import { PostPageComponent } from "@/components/app/posts/post-page";
 import client from "@/tina/__generated__/client";
 import { notFound } from "next/navigation";
@@ -16,3 +16,38 @@ params: { slug: string } }) {
     })
     return <PostPageComponent {...result} />
   }
+*/
+import { PageComponent } from "@/components/app/page";
+import client from "@/tina/__generated__/client";
+import { notFound } from "next/navigation";
+
+// This function fetches all possible slugs for static generation
+export async function generateStaticParams() {
+  try {
+    // Fetch a list of slugs from your data source
+    const slugs = await client.queries..post({ relativePath: `${params.slug}.mdx` })
+    .then((result) =>{
+       return result
+      }); // Replace with your actual query to fetch slugs
+
+    // Return an array of objects with `slug` properties
+    return slugs.map((slug: string) => ({
+      slug: slug,
+    }));
+  } catch (error) {
+    console.error("Failed to fetch slugs", error);
+    return [];
+  }
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const result = await client.queries
+    .page({ relativePath: `${params.slug}.mdx` })
+    .then((result) => result)
+    .catch((err) => {
+      console.error(err);
+      return notFound();
+    });
+
+  return <PageComponent {...result} />;
+}
